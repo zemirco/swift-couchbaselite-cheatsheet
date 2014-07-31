986c3f60ca99b486e496db9e70960f48b7161a38
zeMirco <mirco.zeiss@gmail.com>
2014-07-31 21:01:55 +0200

    add sample xcode 6 beta 4 project

--- swift-couchbaselite/swift-couchbaselite/AppDelegate.swift
+++ swift-couchbaselite/swift-couchbaselite/AppDelegate.swift
@@ -16,8 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: NSDictionary?) -> Bool {
         self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
-        // Override point for customization after application launch.
         self.window!.backgroundColor = UIColor.whiteColor()
+        self.window!.rootViewController = MainViewController()
         self.window!.makeKeyAndVisible()
         return true
     }
--- swift-couchbaselite/swift-couchbaselite/MainViewController.swift
+++ swift-couchbaselite/swift-couchbaselite/MainViewController.swift
@@ -1,10 +1,3 @@
-//
-//  MainViewController.swift
-//  swift-couchbaselite
-//
-//  Created by Zeiss, Mirco on 28.07.14.
-//  Copyright (c) 2014 zemirco. All rights reserved.
-//
 
 import UIKit
 
@@ -12,24 +5,61 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // open db
+        var manager = CBLManager.sharedInstance()
+        
+        var error: NSError?
+        var db = manager.databaseNamed("mydb", error: &error)
+        if error {
+            println(error)
+        }
+        
+        // save document
+        var properties = [
+            "name": "mirco",
+            "email": "mirco.zeiss@gmail.com",
+            "repo": "swift-couchbaselite-cheatsheet"
+        ]
+        
+        var doc = db.createDocument()
+        doc.putProperties(properties, error: &error)
+        if error {
+            println(error)
+        }
+        
+        // Creating and initializing views
+        var view = db.viewNamed("name")
+        var block: CBLMapBlock = { (doc: [NSObject: AnyObject]!, emit: CBLMapEmitBlock!) in
+            println(doc)
+            emit(doc["name"], nil)
+            if doc["name"] {
+                if doc["name"] as NSString == "name" {
+                    emit([doc["repo"] as NSString, doc["name"] as NSString], nil)
+                }
+            }
+        }
+        
+        view.setMapBlock(block, version: "4")
+        
+        // Querying views
+        var query = db.viewNamed("name").createQuery()
+        query.keys = ["mirco"]
+        var result = query.run(&error)
+        if error {
+            println(error)
+        }
+        
+        var count = Int(result.count)
+        
+        for var index = 0; index < count; ++index {
+            println(result.rowAtIndex(UInt(index)).document)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
